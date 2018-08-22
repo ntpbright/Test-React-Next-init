5b971955916f268a78e2240ad367219f500cb9f9
ntpbright <nuttapatprom.c@ku.com>
2018-08-22 09:51:18 +0700

    Add some enviromnmennt

---
+++ .next/4.7cdfbdd48e224249ef35.hot-update.js
@@ -0,0 +1,191 @@
+webpackHotUpdate(4,{
+
+/***/ "./node_modules/antd/lib/button/style/css.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__("./node_modules/antd/lib/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/button/style/index.css");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/button/style/index.css":
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: Unexpected token (5:0)\nYou may need an appropriate loader to handle this file type.\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors */\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors,string-no-newline */\n| .ant-btn {\n|   line-height: 1.5;\n|   display: inline-block;");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/date-picker/style/css.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__("./node_modules/antd/lib/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/date-picker/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/input/style/css.js");
+
+__webpack_require__("./node_modules/antd/lib/time-picker/style/css.js");
+
+__webpack_require__("./node_modules/antd/lib/tag/style/css.js");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/date-picker/style/index.css":
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: Unexpected token (5:0)\nYou may need an appropriate loader to handle this file type.\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors */\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors,string-no-newline */\n| .ant-calendar-picker-container {\n|   font-size: 14px;\n|   font-variant: tabular-nums;");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/input/style/css.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__("./node_modules/antd/lib/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/input/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/button/style/css.js");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/input/style/index.css":
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: Unexpected token (5:0)\nYou may need an appropriate loader to handle this file type.\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors */\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors,string-no-newline */\n| .ant-input {\n|   font-family: \"Chinese Quote\", -apple-system, BlinkMacSystemFont, \"Segoe UI\", \"PingFang SC\", \"Hiragino Sans GB\", \"Microsoft YaHei\", \"Helvetica Neue\", Helvetica, Arial, sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\";\n|   font-variant: tabular-nums;");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/style/index.css":
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: Unexpected character '@' (6:0)\nYou may need an appropriate loader to handle this file type.\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors,string-no-newline */\n| /* stylelint-disable at-rule-no-unknown */\n| @font-face {\n|   font-family: \"Chinese Quote\";\n|   src: local(\"PingFang SC\"), local(\"SimSun\");");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/tag/style/css.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__("./node_modules/antd/lib/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/tag/style/index.css");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/tag/style/index.css":
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: Unexpected token (5:0)\nYou may need an appropriate loader to handle this file type.\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors */\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors,string-no-newline */\n| .ant-tag {\n|   font-family: \"Chinese Quote\", -apple-system, BlinkMacSystemFont, \"Segoe UI\", \"PingFang SC\", \"Hiragino Sans GB\", \"Microsoft YaHei\", \"Helvetica Neue\", Helvetica, Arial, sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\";\n|   font-size: 14px;");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/time-picker/style/css.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__("./node_modules/antd/lib/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/time-picker/style/index.css");
+
+__webpack_require__("./node_modules/antd/lib/input/style/css.js");
+
+/***/ }),
+
+/***/ "./node_modules/antd/lib/time-picker/style/index.css":
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: Unexpected token (5:0)\nYou may need an appropriate loader to handle this file type.\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors */\n| /* stylelint-disable declaration-bang-space-before,no-duplicate-selectors,string-no-newline */\n| .ant-time-picker-panel {\n|   font-size: 14px;\n|   font-variant: tabular-nums;");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("./node_modules/react/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_antd__ = __webpack_require__("./node_modules/antd/es/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_antd_lib_date_picker_style_css__ = __webpack_require__("./node_modules/antd/lib/date-picker/style/css.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_antd_lib_date_picker_style_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_antd_lib_date_picker_style_css__);
+var _jsxFileName = "/Users/nuttapatpromchongamorkulprapa/Documents/work/metromerce/Hackaton/test-react-ui-framework/pages/index.js";
+
+
+
+
+var Index = function Index() {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, "Hello Next.js"), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_antd__["a" /* Button */], {
+    type: "primary",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, "Primary"), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_antd__["a" /* Button */], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    }
+  }, "Default"), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_antd__["a" /* Button */], {
+    type: "dashed",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    }
+  }, "Dashed"), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_antd__["a" /* Button */], {
+    type: "danger",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  }, "Danger"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Index);
+    (function (Component, route) {
+      if(!Component) return
+      if (false) return
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__("./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=4.7cdfbdd48e224249ef35.hot-update.js.map
---
+++ pages/about.js
@@ -0,0 +1,7 @@
+const About = () => (
+  <div>
+    <p>About.js</p>
+  </div>
+)
+
+export default About
